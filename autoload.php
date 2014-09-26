4ae046bc3d1a3275abf66c44c57c01ca6a4961ed
Hari KT <kthari85@gmail.com>
2014-09-26 21:04:17 +0530

    Reduce the autoload functionality

--- autoload.php
+++ autoload.php
@@ -1,36 +1,17 @@
 <?php
 spl_autoload_register(function ($class) {
-    // what prefixes should be recognized?
-    $prefixes = array(
-        "Modler\\" => array(
-            __DIR__ . '/src/Modler',
-            __DIR__ . '/tests/Modler',
-        )
+    $dirs = array(
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
     );
 
-    // go through the prefixes
-    foreach ($prefixes as $prefix => $dirs) {
-
-        // does the requested class match the namespace prefix?
-        $prefix_len = strlen($prefix);
-        if (substr($class, 0, $prefix_len) !== $prefix) {
-            continue;
-        }
-
-        // strip the prefix off the class
-        $class = substr($class, $prefix_len);
-
+    foreach ($dirs as $dir) {
         // a partial filename
         $part = str_replace('\\', DIRECTORY_SEPARATOR, $class) . '.php';
-
-        // go through the directories to find classes
-        foreach ($dirs as $dir) {
-            $dir = str_replace('/', DIRECTORY_SEPARATOR, $dir);
-            $file = $dir . DIRECTORY_SEPARATOR . $part;
-            if (is_readable($file)) {
-                require_once $file;
-                return;
-            }
+        $file = $dir . DIRECTORY_SEPARATOR . $part;
+        if (is_readable($file)) {
+            require_once $file;
+            return;
         }
     }
 
